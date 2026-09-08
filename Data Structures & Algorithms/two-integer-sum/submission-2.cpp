0f091d9cda419e6338476a830dc67bdfb2d8399f
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2026-09-09 01:06:51 +0900

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Exactly one pair
+
+        // Hashmap would be best
+        // O(1) * O(n) TC
+        // O(n) SC
+
+        unordered_map<int, int> num_idx;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int diff = target - num;
+            if (num_idx.contains(diff)) {
+                return {num_idx[diff], i};
+            }
+            
+            num_idx[num] = i;
+        }
+
+        return {-1, -1};
+    }
+};
+
