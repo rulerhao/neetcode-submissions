9cfbe409afe545f44e882f77393b421c003c203c
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2026-09-10 23:16:14 +0900

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // # Lowercase English Letters
+        
+        // n: vector size
+        // m: str size.
+        // Add one str to count table: O(m)
+        // Add all strs to count table: n * O(m)
+        // The max count table size would be n(Because all distinct)
+        // forEach (strs) to search same table to put in array: n x n -> O(n^2)
+
+        map<array<int, 26>, vector<string>> count_tables;
+
+        for (int i = 0; i < strs.size(); i++) {
+            string str = strs[i];
+            array<int, 26> count_table = {0};
+            for (int j = 0; j < str.size(); j++) {
+                char ch = str[j];
+                count_table[ch - 'a'] = count_table[ch - 'a'] + 1;
+            }
+
+            count_tables[count_table].push_back(str);
+        }
+
+        vector<vector<string>> res;
+
+        for (auto& [count_table, str_list] : count_tables) {
+            res.push_back(str_list);
+        }
+
+        return res;
+    }
+};
